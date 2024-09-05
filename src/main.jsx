cf1b42651dd0c1597240281e88ff0970d562e9a3
kimzoo-second <qwbvc@naver.com>
2024-09-05 13:04:02 +0900

    added comments

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// this is important!
+// this is important!!
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
